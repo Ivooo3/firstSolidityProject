34ba4bee0f2ded39563c814b8c6f4344993dfa12
ITopencharov <topencharov@gmail.com>
2022-09-06 08:15:13 +0300

    deploy contract to the blockchain.

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,36 +1,25 @@
 const assert = require("assert");
 const ganache = require("ganache-cli");
-//its with capital letter because its constructor
-//when using Web3 we will need a provider like ganache for example
 const Web3 = require("web3");
-//lower case means instance
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require("../compile");
 
-class Car {
-  park() {
-    return "stopped";
-  }
+let accounts;
+let inbox;
 
-  drive() {
-    return "vroom";
-  }
-}
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
 
-//describe is used to contain batch of testing funcions
-//first parameter is title of the set of tests
-//second param is a function which executes functions which actually tests the code
-describe("Car", () => {
-  let car;
-  beforeEach(() => {
-    car = new Car();
-  });
+  inbox = new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi there!"],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
+});
 
-  it("can park", () => {
-    //first is the param containing the value generated from the code and the second is the value of the wanted result
-    assert.equal(car.park(), "stopped");
-  });
-
-  it("can drive", () => {
-    assert.equal(car.drive(), "vroom");
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    console.log(inbox);
   });
 });
